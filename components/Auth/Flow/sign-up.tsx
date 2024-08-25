86557d998388ccefbb9402fb1aed4272632f8c9f
Dada Teniola <emmatenny2004@gmail.com>
2024-08-25 16:45:28 +0100

    changed password field for sign up and create new password

--- components/Auth/Flow/sign-up.tsx
+++ components/Auth/Flow/sign-up.tsx
@@ -27,34 +27,34 @@ const SignUp: React.FC<FlowComponentProps> = ({ changeStep }) => {
 
   return (
     <AuthForm
-      onFormSubmit={handleSignUp} // Callback function to handle form submission
-      setValidationErrors={setErrorMsgs} // Function to set validation error messages
-      className="custom-flex-col gap-10" // Additional class names for styling
+      onFormSubmit={handleSignUp}
+      setValidationErrors={setErrorMsgs}
+      className="custom-flex-col gap-10"
     >
       <AuthHeading title="Create Your Account">
         Please provide the following information to set up your account.
       </AuthHeading>
       <div className="custom-flex-col gap-6">
         <Input
-          id="email" // Input field ID, used for form handling and accessibility
-          type="email" // Specifies that the input is for email addresses
-          label="email" // Label text for the input field
-          placeholder="Email Address" // Placeholder text for the input field
-          validationErrors={errorMsgs} // Passes validation errors to the input component
+          id="email"
+          type="email"
+          label="email"
+          placeholder="Email Address"
+          validationErrors={errorMsgs}
         />
         <p className="text-text-label text-base font-normal">
           Passwords must contain an UPPERCASE letter, a lowercase letter, a
           number, and a special character.
         </p>
         <div className="custom-flex-col gap-4">
-          <AuthNewPassword validationErrors={errorMsgs} />
           {/* Component to handle password input and strength validation */}
+          <AuthNewPassword validationErrors={errorMsgs} />
           <Input
-            id="confirm-password" // Input field ID
-            type="password" // Specifies that the input is for password
-            label="confirm password" // Label text for the input field
-            placeholder="Write here" // Placeholder text for the input field
-            validationErrors={errorMsgs} // Passes validation errors to the input component
+            id="confirm-password"
+            type="password"
+            label="confirm password"
+            placeholder="Write here"
+            validationErrors={errorMsgs}
           />
         </div>
       </div>
@@ -64,7 +64,7 @@ const SignUp: React.FC<FlowComponentProps> = ({ changeStep }) => {
           Got an account?
         </AuthAction>
         {/* Button to submit the form */}
-        <Button type="submit">continue</Button>{" "}
+        <Button type="submit">continue</Button>
       </div>
     </AuthForm>
   );
