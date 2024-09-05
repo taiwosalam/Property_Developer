ac28ab73e1322c1c8dec97711f157834f3592899
onos <eonosereme@gmail.com>
2024-09-05 19:40:53 +0100

    d

---
+++ app/(nav)/management/staff-branch/types.d.ts
@@ -0,0 +1,8 @@
+export interface StaffAndBranchState {
+  gridView: boolean;
+  total_pages: number;
+  current_page: number;
+  selectedState: string;
+  selectedLGA: string;
+  localGovernments: string[];
+}
