330b80ffac149a8a68a46fedd0079cfb951a4c64
onos <eonosereme@gmail.com>
2024-09-06 15:49:47 +0100

    .

--- components/Management/add-landlord-or-tenant-card.tsx
+++ components/Management/add-landlord-or-tenant-card.tsx
@@ -16,9 +16,16 @@ const AddLandlordOrTenantCard: React.FC<AddLandlordOrTenantCardProps> = ({
 }) => {
   return (
     <div className="w-[250px] h-[250px] flex gap-5 flex-col text-center items-center justify-center rounded-2xl border border-solid border-brand-9 bg-neutral-2">
-      <p className="text-brand-9 text-xl font-medium capitalize">{title}</p>
-      <p className="text-[#606060] text-xs font-normal">{desc}</p>
-      <Button onClick={onClick} size="base_medium" className="py-2 px-8">
+      <p className="text-brand-9 text-xl font-medium capitalize mb-4">
+        {title}
+      </p>
+      <p className="text-[#606060] text-xs font-normal mb-8">{desc}</p>
+      <Button
+        type="button"
+        onClick={onClick}
+        size="base_medium"
+        className="py-2 px-8"
+      >
         {buttonText}
       </Button>
     </div>
