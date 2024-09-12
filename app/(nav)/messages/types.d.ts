05d9bc77850fc8d48805861e6b3f3264f4bdf253
onos <eonosereme@gmail.com>
2024-09-12 11:04:00 +0100

    fixed image sliders

--- app/(nav)/messages/types.d.ts
+++
@@ -1,3 +0,0 @@
-export interface MessagesLayoutProps {
-  children: React.ReactNode;
-}
--- app/auth/types.d.ts
+++
@@ -1,9 +0,0 @@
-export interface AuthLayoutProps {
-  children: React.ReactNode;
-}
-
-export interface AuthFlowLayoutProps {
-  children: React.ReactNode;
-}
-
-export type FormSteps = "next" | "prev";
