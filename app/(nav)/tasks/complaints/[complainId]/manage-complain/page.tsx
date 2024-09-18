270ff261bb9268aea6bf956eb79f054e8fdd0079
weird-samuel <fiyinfoluwasamuel580@gmail.com>
2024-09-18 20:48:10 +0100

    ...

--- app/(nav)/tasks/complaints/[complainId]/manage-complain/page.tsx
+++ app/(nav)/tasks/complaints/[complainId]/manage-complain/page.tsx
@@ -1,5 +1,29 @@
+import AboutTaskCard from "@/components/tasks/complainid/about-task-card";
+import AttchedImagesGrid from "@/components/tasks/complainid/Attached-images-grid";
+
 const ManageComplain = () => {
-  return <div>ManageComplain</div>;
+  const images = [
+    "/empty/SampleProperty.jpeg",
+    "/empty/SampleProperty2.jpeg",
+    "/empty/SampleProperty3.jpeg",
+    "/empty/SampleProperty4.png",
+    "/empty/SampleProperty5.jpg",
+    "/empty/SampleProperty6.jpg",
+  ];
+  return (
+    <section className="w-full lg:flex lg:items-start lg:gap-x-10">
+      <div className="w-full lg:w-3/5 lg:h-full">
+        <div className="w-full h-full space-y-10">
+          <AboutTaskCard />
+        </div>
+      </div>
+      <div className="w-full lg:w-2/5 lg:h-full">
+        <div className="w-full h-full space-y-10">
+          <AttchedImagesGrid images={images} />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ManageComplain;
---
+++ components/tasks/complainid/Attached-images-grid.tsx
@@ -0,0 +1,58 @@
+import { ChevronLeft } from "@/public/icons/icons";
+import Image from "next/image";
+
+interface AttachedImagesGridProps {
+  images: string[];
+}
+
+const AttachedImagesGrid: React.FC<AttachedImagesGridProps> = ({ images }) => {
+  return (
+    <div className="space-y-4">
+      <div className="w-full flex items-center justify-between">
+        <h1 className="text-[16px] font-medium">Attached Images</h1>
+        <div className="flex items-center space-x-2 text-text-label text-sm font-medium">
+          <p>Hide Images</p>
+          <div className="-rotate-90">
+            <ChevronLeft fill="#5A5D61" />
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-2 gap-4">
+        {images.slice(0, 3).map((src, index) => (
+          <div
+            key={index}
+            className="w-full md:h-[200px] h-[120px] xl:h-[115px] lg:h-[140px] bg-gray-200 rounded-lg relative overflow-hidden"
+          >
+            <Image
+              src={src}
+              alt={`image-${index}`}
+              fill
+              sizes="auto"
+              priority
+              className="object-cover object-center"
+              quality={60}
+            />
+          </div>
+        ))}
+        {images.length > 3 && (
+          <div className="w-full md:h-[200px] h-[120px] xl:h-[115px] lg:h-[140px] bg-gray-200 rounded-lg relative overflow-hidden">
+            <div className="absolute inset-0 bg-black bg-opacity-60 flex items-center justify-center z-10">
+              <span className="text-xl text-white">+ {images.length - 3}</span>
+            </div>
+            <Image
+              src={images[3]}
+              alt="image-3"
+              fill
+              sizes="auto"
+              priority
+              className="object-cover object-center"
+              quality={60}
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AttachedImagesGrid;
