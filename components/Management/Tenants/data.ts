9680b8f30a9ad119ee2027c99d1d8dad89736d0e
Dada Teniola <emmatenny2004@gmail.com>
2024-09-23 16:33:14 +0100

    minor jchange

--- components/Management/Tenants/data.ts
+++ components/Management/Tenants/data.ts
@@ -1,22 +1,37 @@
+import { toast } from "sonner";
+
 export const addTenant = async (formData: any, accessToken: string | null) => {
   try {
+    const errors: string[] = [];
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_BASE_URL}/tenants`,
       {
         method: "POST",
         body: formData,
         headers: {
-          "Content-Type": "application/json",
           Authorization: `Bearer ${accessToken}`,
         },
       }
-    ).then((res) => res.json());
+    );
 
-    console.log(response);
+    const data = await response.json();
+
+    if (!response.ok) {
+      Object.values(data.errors).forEach((error, index) => {
+        errors.push(error as string);
+      });
+
+      console.log(errors);
+
+      toast.error(errors.join(", "));
+    }
+
+    console.log(response, data);
   } catch (error) {
     console.error("Error adding tenant:", error);
   }
 };
+
 export const addMultipleTenants = async (formData: any) => {
   try {
     const response = await fetch(
