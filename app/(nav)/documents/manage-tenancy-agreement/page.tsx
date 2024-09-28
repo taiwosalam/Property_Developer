981e64fcd87896f53458f3c005866fec07896697
Dada Teniola <emmatenny2004@gmail.com>
2024-09-28 23:44:06 +0100

    create and manage tenancy agreements pages complete

---
+++ app/(nav)/documents/manage-tenancy-agreement/page.tsx
@@ -0,0 +1,94 @@
+import React from "react";
+
+// Imports
+import Button from "@/components/Form/Button/button";
+import BackButton from "@/components/BackButton/back-button";
+import KeyValueList from "@/components/KeyValueList/key-value-list";
+import { SectionSeparator } from "@/components/Section/section-components";
+import { Modal, ModalContent, ModalTrigger } from "@/components/Modal/modal";
+import DeleteDocumentModal from "@/components/Documents/delete-document-modal";
+import DocumentTenancyAgreements from "@/components/Documents/document-tenancy-agreements";
+import { LandlordTenantInfoBox } from "@/components/Management/landlord-tenant-info-components";
+
+const ManageTenancyAgreement = () => {
+  return (
+    <div className="custom-flex-col gap-10 pb-[100px]">
+      <div className="custom-flex-col gap-6">
+        <BackButton>Manage Tenancy Agreement</BackButton>
+        <LandlordTenantInfoBox className="custom-flex-col gap-[10px]">
+          <h2 className="text-primary-navy text-xl font-bold">
+            Property Details
+          </h2>
+          <SectionSeparator />
+          <div className="flex">
+            <KeyValueList
+              data={{}}
+              chunkSize={2}
+              referenceObject={{
+                "property description": "",
+                "property address": "",
+                "annual rent": "",
+                "caution deposit": "",
+              }}
+            />
+          </div>
+        </LandlordTenantInfoBox>
+        <LandlordTenantInfoBox className="custom-flex-col gap-[10px]">
+          <h2 className="text-primary-navy text-xl font-bold">
+            Landlord/Landlady Details
+          </h2>
+          <SectionSeparator />
+          <div className="flex">
+            <KeyValueList
+              data={{}}
+              chunkSize={2}
+              referenceObject={{
+                "Landlord/Landlady Name": "",
+                "Landlord/Landlady ID": "",
+                "Landlord/Landlady Address": "",
+                "account type": "",
+              }}
+            />
+          </div>
+        </LandlordTenantInfoBox>
+      </div>
+      <div className="custom-flex-col gap-8">
+        <div className="custom-flex-col gap-4">
+          <div className="custom-flex-col gap-1">
+            <h2 className="text-primary-navy text-xl font-bold">
+              Customized Agreement
+            </h2>
+            <p className="text-text-disabled text-sm font-normal">
+              Please choose any options below that are most applicable to the
+              property agreement.
+            </p>
+          </div>
+          <SectionSeparator />
+        </div>
+        <DocumentTenancyAgreements />
+      </div>
+      <div className="fixed bottom-0 right-0 w-full bg-white py-5 px-[60px] flex gap-6 justify-end">
+        <Modal>
+          <ModalTrigger asChild>
+            <Button variant="light_red" size="base_bold" className="py-2 px-6">
+              delete account
+            </Button>
+          </ModalTrigger>
+          <ModalContent>
+            <DeleteDocumentModal />
+          </ModalContent>
+        </Modal>
+        <div className="flex gap-6">
+          <Button variant="sky_blue" size="base_bold" className="py-2 px-6">
+            print & preview
+          </Button>
+          <Button size="base_bold" className="py-2 px-6">
+            save as draft
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ManageTenancyAgreement;
