6bdef26bed980b6847e5c10e3fa1bcfc0a107322
weird-samuel <fiyinfoluwasamuel580@gmail.com>
2024-09-30 10:39:34 +0100

    create invoice completed

--- components/Accounting/invoice/create-invoice/Details.tsx
+++ components/Accounting/invoice/create-invoice/Details.tsx
@@ -5,7 +5,7 @@ const Details = () => {
       style={{ boxShadow: "4px 4px 20px 2px rgba(0, 0, 0, 0.02)" }}
     >
       <h1 className="font-bold text-xl text-[#092C4C]">Details</h1>
-      <div className="h-[2px] w-3/4 bg-[#C0C2C8]" />
+      <div className="h-[2px] w-3/4 bg-[#C0C2C8] bg-opacity-20" />
       <div className="w-full md:w-3/4 flex items-center flex-wrap space-x-10 text-[16px] font-normal text-black">
         <div className="flex items-center space-x-10">
           <p className="text-[#747474]">Property Id</p>
