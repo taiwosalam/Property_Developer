854d6f87c570ff843c8d1b4c2bbd704a358fc34e
Dada Teniola <emmatenny2004@gmail.com>
2024-10-03 16:52:15 +0100

    statement first page complete

--- components/Dropdown/types.d.ts
+++ components/Dropdown/types.d.ts
@@ -14,5 +14,6 @@ export interface DropdownTriggerProps
 
 export interface DropdownContentProps
   extends React.HTMLAttributes<HTMLDivElement> {
+  direction?: "up" | "down";
   position?: "left" | "right";
 }
