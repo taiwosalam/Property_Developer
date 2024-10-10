189a190f8d67c70b58324cd196449665e23612b1
mubarakbdlrf <mubarakbdlrf@gmail.com>
2024-10-10 18:54:07 +0000

    Done with settings/others page

--- components/Settings/settings-components.tsx
+++ components/Settings/settings-components.tsx
@@ -60,10 +60,10 @@ export const SettingsOthersCheckBox: React.FC<SettingsOthersCheckBoxProps> = ({ 
       <p className="text-text-disabled text-sm font-normal max-w-[900px]"> { desc } </p>
     </div>
 
-    <div className="second flex justify-end">
+    <div className="second flex justify-end items-center">
       <label className="relative inline-flex items-center cursor-pointer">
         <input type="checkbox" className="sr-only peer" />
-        <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-3 after:left-[1px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-status-success-primary" />
+        <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[1px] after:left-[1px] after:bg-white after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-status-success-primary" />
       </label>
     </div>
   </div>
