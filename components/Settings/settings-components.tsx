16b24119f0f288bd1fe5db2841fb24624cbe3f84
mubarakbdlrf <mubarakbdlrf@gmail.com>
2024-10-11 14:57:37 +0000

    Conflict fix

--- components/Settings/settings-components.tsx
+++ components/Settings/settings-components.tsx
@@ -102,7 +102,6 @@ export const DirectorCard: React.FC<SettingsDirectorTypes> = ({ name, email, pos
   </div>
 )
 
-<<<<<<< HEAD
 export const ThemeCard: React.FC<ThemeCardProps> = ({ img, value, onSelect }) => {
   return (
     <div
@@ -122,97 +121,3 @@ export const ThemeCard: React.FC<ThemeCardProps> = ({ img, value, onSelect }) =>
   );
 };
 
-
-
-
-=======
-export const ThemeCard: React.FC<ThemeCardProps> = ({ img, value, onSelect }) => (
-  <div
-    className="themesWrapper mt-4 flex items-center flex-wrap gap-4 cursor-pointer"
-    onClick={() => onSelect(value)} // Call onSelect with the value when clicked
-  >
-    <div className="imgWrapper w-[218px] h-[218px]">
-      <Image
-        src={img}
-        alt="Theme"
-        width={1000}
-        height={1000}
-        className="w-full h-full object-contain"
-      />
-    </div>
-  </div>
-);
-
-
->>>>>>> 541075f (Added Appearance page)
-// GITHUB ISSUES
-
-// export const SettingsOthersType: React.FC<SettingsOthersProps> = ({ title, desc, icon }) => (
-//   <div className="flex justify-between">
-//     <div className="first flex gap-1 items-start">
-//       <Image
-//         src={icon}
-//         width={24}
-//         height={24}
-//         alt='user'
-//       />
-//       <div className="flex flex-col">
-//         <h4 className="text-text-quaternary text-base"> {title} </h4>
-//         <p className="text-text-disabled text-sm font-normal max-w-[900px]"> {desc} </p>
-//       </div>
-//     </div>
-
-//     <div className="second flex justify-end">
-//       <input type="checkbox" />
-//     </div>
-//   </div>
-// )
-
-// export const SettingsOthersCheckBox: React.FC<SettingsOthersCheckBoxProps> = ({ title, desc }) => (
-//   <div className="flex justify-between">
-//     <div className="flex flex-col">
-//       <h4 className="text-text-quaternary text-base"> {title} </h4>
-//       <p className="text-text-disabled text-sm font-normal max-w-[900px]"> { desc } </p>
-//     </div>
-
-//     <div className="second flex justify-end items-center">
-//       <label className="relative inline-flex items-center cursor-pointer">
-//         <input type="checkbox" className="sr-only peer" />
-//         <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[1px] after:left-[1px] after:bg-white after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-status-success-primary" />
-//       </label>
-//     </div>
-//   </div>
-// )
-
-
-// export const DirectorCard: React.FC<SettingsDirectorTypes> = ({ name, email, position, img, phone, icon }) => (
-//   <div className="card p-2 flex justify-between max-w-[397px] border rounded-md bg-[#F9F9F9] border-brand-tertiary">
-//     <div className="flex items-center gap-4">
-//       <div className="imageWrapper max-h-[120px] max-w-[120px] rounded-md">
-//         <Image
-//           src={img}
-//           alt='User Pics'
-//           width={500}
-//           height={500}
-//           className="w-full h-full object-contain rounded-md"
-//         />
-//       </div>
-//       <div className="flex flex-col relative">
-//         <div className="flex gap-2 items-center">
-//           <h4 className="text-black font-bold leading-3"> {name} </h4>
-//           <Image
-//             alt="companyy_irector"
-//             src={`${icon ? icon : '/icons/unverified.svg'}`}
-//             width={20}
-//             height={20}
-//             className='object-contain'
-//           />
-//         </div>
-//         <p className="text-sm"> {email} </p>
-//         {position && <p className="my-4 text-base text-brand-10"> {position} </p>}
-//         {(!position || position === "") && <div className="py-1 my-2 max-w-[100px] bg-status-success-1 rounded-md text-center text-status-success-3">mobile</div>}
-//         <p className="text-md text-[#8D8D8D]"> {phone} </p>
-//       </div>
-//     </div>
-//   </div>
-// )
