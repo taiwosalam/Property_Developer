e9f8bb875d228e63d6c03d44abd58691737136ce
try-samuel <fiyinfoluwasamuel580@gmail.com>
2024-10-14 11:47:50 +0100

    sidenav update, only one dropdown will be active at a time

--- components/Nav/sidenav.tsx
+++ components/Nav/sidenav.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 
@@ -15,6 +15,11 @@ import { NavButton } from "./nav-components";
 
 const Sidenav: React.FC<SidenavProps> = ({ showLogo, closeSidenav }) => {
   const pathname = usePathname();
+  const [activeDropdown, setActiveDropdown] = useState<string | null>(null);
+
+  const handleDropdownToggle = (label: string) => {
+    setActiveDropdown(prevActive => prevActive === label ? null : label);
+  };
 
   return (
     <div className="custom-flex-col w-[250px] m pb-3in-w-[250px]">
@@ -30,13 +35,15 @@ const Sidenav: React.FC<SidenavProps> = ({ showLogo, closeSidenav }) => {
       {nav_items.map((item, idx) =>
         item.content ? (
           <NavDropdown
-            onContentClick={closeSidenav}
-            highlight={item.content.some((i) =>
-              pathname.includes(`${item.label}${i.href}`)
-            )}
             key={idx}
             type={item.type}
             content={item.content}
+            highlight={item.content.some((i) =>
+              pathname.includes(`${item.label}${i.href}`)
+            )}
+            onContentClick={closeSidenav}
+            isOpen={activeDropdown === item.label}
+            onToggle={() => handleDropdownToggle(item.label)}
           >
             {item.label}
           </NavDropdown>
