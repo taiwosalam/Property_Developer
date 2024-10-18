4e5e1bb876c61ba3a72056ffcc93c907bf9a6c3a
Binary-Tech001 <mubarakbdlrf@gmail.com>
2024-10-18 09:47:23 +0100

    Settings/appearance fixes

--- app/(nav)/settings/appearance/page.tsx
+++ app/(nav)/settings/appearance/page.tsx
@@ -270,11 +270,8 @@ import { website_color_schemes } from "@/components/Settings/data";
 import { Modal, ModalContent, ModalTrigger } from "@/components/Modal/modal";
 import { useThemeStoreSelectors } from "@/store/themeStore";
 import { rgbToHex } from "@/utils/rgbaToHex";
-<<<<<<< HEAD
 import { Tooltip } from "@mui/material";
-=======
 import { hexToRgb } from "@/utils/rgbaToHex";
->>>>>>> d480ff44710fa1f2d0828c0a0984830d56a93962
 
 const Appearance = () => {
   const setColor = useThemeStoreSelectors.getState().setColor;
