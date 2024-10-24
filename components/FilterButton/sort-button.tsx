c0fc2711ebf06cf56c316fc8f8b58becbf69c5b5
try-samuel <fiyinfoluwasamuel580@gmail.com>
2024-10-24 12:27:25 +0100

    sort button design implemented with last icon

--- components/FilterButton/sort-button.tsx
+++ components/FilterButton/sort-button.tsx
@@ -1,22 +1,53 @@
-import { ArrowDownAZ, ArrowUpZA } from "lucide-react";
+import { CalendarIcon } from "@/public/icons/icons";
+import { ArrowDownAZ, ArrowUpZA, CalendarDays } from "lucide-react";
 import { useState } from "react";
 
 const SortButton = () => {
   const [sort, setSort] = useState("asc");
+
   const toggleSort = () => {
-    setSort(sort === "asc" ? "desc" : "asc");
+    setSort((current) => {
+      switch (current) {
+        case "asc":
+          return "desc";
+        case "desc":
+          return "date";
+        default:
+          return "asc";
+      }
+    });
   };
+
+  const getIcon = () => {
+    switch (sort) {
+      case "asc":
+        return (
+          <ArrowDownAZ
+            size={20}
+            className="text-[#344054] dark:text-darkText-1"
+          />
+        );
+      case "desc":
+        return (
+          <ArrowUpZA
+            size={20}
+            className="text-[#344054] dark:text-darkText-1"
+          />
+        );
+      case "date":
+        return <CalendarIcon />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <button
       type="button"
       className="bg-white dark:bg-darkText-primary rounded-lg p-2 flex items-center gap-2"
       onClick={toggleSort}
     >
-      {sort === "asc" ? (
-        <ArrowDownAZ size={20} className="text-[#344054] dark:text-darkText-1" />
-      ) : (
-        <ArrowUpZA size={20} className="text-[#344054] dark:text-darkText-1" />
-      )}
+      {getIcon()}
       <span className="text-[#344054] dark:text-darkText-1 text-base font-medium">
         Sort
       </span>
