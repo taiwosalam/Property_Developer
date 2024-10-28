0eb8ab57a846cb6cb21bc04487b8ce3f41bcd0c0
Binary-Tech001 <mubarakbdlrf@gmail.com>
2024-10-28 09:15:16 +0100

    agent community

---
+++ app/(nav)/tasks/agent-community/data.ts
@@ -0,0 +1,153 @@
+import { empty } from "@/app/config";
+import { title } from "process";
+import { CommentProps } from "./type";
+
+export const threadData = [
+  {
+    picture_url: "/empty/thread.png" || empty,
+    user_pics: "/empty/thread.svg" || empty,
+    name: "Esq Salam AIshat",
+    role: "Legal Practitioner",
+    time: '3 mins ago',
+    title: 'Rent Increase & Maintenance',
+    desc: 'Lorem ipsum dolor sit amet consectetur. Id dolor consectetur amet eget at accumsan. Vulputate aenean justo tellus egestas vestibulum phasejsjdbksjbskbllus...',
+    comments: '30 Comments',
+  },
+  {
+    picture_url: "/empty/thread.png" || empty,
+    user_pics: "/empty/thread.svg" || empty,
+    name: "Esq Salam AIshat",
+    role: "Legal Practitioner",
+    time: '3 mins ago',
+    title: 'Rent Increase & Maintenance',
+    desc: 'Lorem ipsum dolor sit amet consectetur. Id dolor consectetur amet eget at accumsan. Vulputate aenean justo tellus egestas vestibulum phasejsjdbksjbskbllus...',
+    comments: '30 Comments',
+  },
+  {
+    picture_url: "/empty/thread.png" || empty,
+    user_pics: "/empty/thread.svg" || empty,
+    name: "Esq Salam AIshat",
+    role: "Legal Practitioner",
+    time: '3 mins ago',
+    title: 'Rent Increase & Maintenance',
+    desc: 'Lorem ipsum dolor sit amet consectetur. Id dolor consectetur amet eget at accumsan. Vulputate aenean justo tellus egestas vestibulum phasejsjdbksjbskbllus...',
+    comments: '30 Comments',
+  },
+  {
+    picture_url: "/empty/thread.png" || empty,
+    user_pics: "/empty/thread.svg" || empty,
+    name: "Esq Salam AIshat",
+    role: "Legal Practitioner",
+    time: '3 mins ago',
+    title: 'Rent Increase & Maintenance',
+    desc: 'Lorem ipsum dolor sit amet consectetur. Id dolor consectetur amet eget at accumsan. Vulputate aenean justo tellus egestas vestibulum phasejsjdbksjbskbllus...',
+    comments: '30 Comments',
+  },
+  {
+    picture_url: "/empty/thread.png" || empty,
+    user_pics: "/empty/thread.svg" || empty,
+    name: "Esq Salam AIshat",
+    role: "Legal Practitioner",
+    time: '3 mins ago',
+    title: 'Rent Increase & Maintenance',
+    desc: 'Lorem ipsum dolor sit amet consectetur. Id dolor consectetur amet eget at accumsan. Vulputate aenean justo tellus egestas vestibulum phasejsjdbksjbskbllus...',
+    comments: '30 Comments',
+  },
+  {
+    picture_url: "/empty/thread.png" || empty,
+    user_pics: "/empty/thread.svg" || empty,
+    name: "Esq Salam AIshat",
+    role: "Legal Practitioner",
+    time: '3 mins ago',
+    title: 'Rent Increase & Maintenance',
+    desc: 'Lorem ipsum dolor sit amet consectetur. Id dolor consectetur amet eget at accumsan. Vulputate aenean justo tellus egestas vestibulum phasejsjdbksjbskbllus...',
+    comments: '30 Comments',
+  },
+  {
+    picture_url: "/empty/thread.png" || empty,
+    user_pics: "/empty/thread.svg" || empty,
+    name: "Esq Salam AIshat",
+    role: "Legal Practitioner",
+    time: '3 mins ago',
+    title: 'Rent Increase & Maintenance',
+    desc: 'Lorem ipsum dolor sit amet consectetur. Id dolor consectetur amet eget at accumsan. Vulputate aenean justo tellus egestas vestibulum phasejsjdbksjbskbllus...',
+    comments: '30 Comments',
+  },
+];
+
+
+
+export const threadArticle = [
+    '#Commercial and retail real estate fundamentals are expected to remain strong due to the scarcity of new construction deliveries, prompting compelling opportunities for investors amid high interest rates and inflation in the market, writes CHINEDUM UWAEGBULAM.',
+    'Despite economic headwinds and challenges with obtaining building permits, experts predict that the demand for housing will remain strong, and the market will see a steady increase in property values this year.',
+    'There are also opportunities available for high-quality properties that meet the needs of investors and tenants, while low mortgage rates and government incentives will likely contribute to this optimistic outlook as inflation may remain a concern in 2024, affecting both home prices and mortgage rates.',
+    'The Guardian gathered that one of the key factors that will shape the real estate market in 2024 is technology, as virtual reality property tours, blockchain in real estate transactions, and Artificial Intelligence (AI)-driven market analysis tools will make the buying and selling process more efficient and accessible ',
+    'Besides, with high demand and limited supply, sellers can anticipate competitive offers and faster sales. They anticipate primary markets becoming more competitive and expensive, secondary markets offering attractive opportunities for buyers and investors looking for affordable properties.',
+    'It is expected that cities and other states’ capitals without many security challenges will witness refinements. Many urban centres will witness positive changes in real estate, but the most prominent among them are Lagos, being the economic hub of the country; Abuja- the political heart of Nigeria; Port Harcourt- the oil and gas industry; Ibadan as a growing metropolis; Abeokuta being the emerging economic centre; Uyo as a blossoming urban centre; and Kano metropolis.',
+]
+
+
+export const comments: CommentProps[] = [
+  {
+    id: 1,
+    name: "Oloruntoba Morakinyo",
+    text: "It is expected that cities and other states’ capitals without many security challenges will witness refinements. Many urban centres will witness positive changes in real estate",
+    likes: 1,
+    dislikes: 0,
+    replies: [
+      {
+        id: 2,
+        name: "John Doe",
+        text: "It is expected that cities and other states capitals without many security challenges",
+        likes: 4,
+        dislikes: 2,
+        replies: [
+          {
+            id: 12,
+            name: "Star Trek",
+            text: "Just making things complicated",
+            likes: 0,
+            dislikes: 0,
+          },
+        ],
+      },
+      {
+        id: 3,
+        name: "Amada Okeke",
+        text: "I disagree with the above statement",
+        likes: 0,
+        dislikes: 12,
+      },
+      {
+        id: 4,
+        name: "John Doe",
+        text: "I agree with the above statement",
+        likes: 4,
+        dislikes: 0,
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: "Cythia Mordi",
+    text: "What are the way we can reduce the rate of unemployment in Nigeria?",
+    likes: 0,
+    dislikes: 0,
+  },
+  {
+    id: 6,
+    name: "Copland Marker",
+    text: "Have you watched Squid Game?",
+    likes: 5,
+    dislikes: 0,
+    replies: [
+      {
+        id: 7,
+        name: "Dont Check My Name",
+        text: "I dont watch boring movies. Hahaha",
+        likes: 500,
+        dislikes: 0,
+      },
+    ],
+  },
+];
