44677d907e6acabb9dea4fb6aa4884a888d972bc
BinaryTech001 <mubarakbdlrf@gmail.com>
2024-10-28 05:22:39 -0700

    agent-community

--- app/(nav)/tasks/agent-community/[threadId]/preview/page.tsx
+++ app/(nav)/tasks/agent-community/[threadId]/preview/page.tsx
@@ -10,8 +10,8 @@ import Sample3 from "@/public/empty/SampleProperty3.jpeg";
 import Sample4 from "@/public/empty/SampleProperty4.png";
 import Sample5 from "@/public/empty/SampleProperty5.jpg";
 import { PropertyImageSlider } from "@/components/Management/Rent And Unit/rental-property-card";
-import { comments, threadArticle } from "../../data";
-import { ChevronLeft, ThumbsDown, ThumbsUp } from "@/public/icons/icons";
+import { comments, companyStats, threadArticle } from "../../data";
+import { ChevronLeft, FbIcon, InstagramIcon, Mail, MapIcon, PhoneIcon, SocialWebIcon, ThumbsDown, ThumbsUp, TwitterIcon, WebsiteIcon } from "@/public/icons/icons";
 import user1 from "@/public/empty/user1.svg";
 import user2 from "@/public/empty/user2.svg";
 import user3 from "@/public/empty/user3.svg";
@@ -198,8 +198,62 @@ const CompanySummary = () => {
   return (
     <div className="bg-white shadow-md dark:bg-darkText-primary p-4 rounded-lg">
       <h2 className="text-black font-semibold text-lg">Company Summary</h2>
-      <div className="flex w-full items-center justify-center w-[246px] h-[48px]">
-        <Image src={CompanyLogo} alt="company logo" width={200} height={200} className="w-full h-full object-contain" />
+      <div className="flex items-center justify-center w-full mt-4">
+      <div className="flex items-center justify-center w-[260px] h-[70px] border border-brand-9 py-2 rounded-lg">
+        <Image src={CompanyLogo} alt="company logo" width={500} height={500} className="w-full h-full object-contain" />
+      </div>
+      </div>
+      <div className="details mt-3">
+        <h3 className="text-black leading-50 font-bold text-6"> Pat Onukwuli & Co. </h3>
+        <div className="service flex flex-col gap-1 mt-1">
+        <p> Services </p>
+        <p className="text-sm text-text-disabled"> Sales, Purchase, Rent of Resident Property & Commercial Property </p>
+        </div>
+        <div className="contacts-details flex flex-col gap-3 mt-4">
+        <p> Contacts </p>
+        <div className="flex gap-2 text-sm text-text-disabled">
+          <MapIcon />
+          <span> States and Local Govt. </span>
+        </div>
+        <div className="flex gap-2 text-sm text-text-disabled">
+          <WebsiteIcon />
+          <span> https://www.hprealestate.co.in </span>
+        </div>
+        <div className="flex gap-2 text-sm text-text-disabled">
+          <PhoneIcon />
+          <span>08132086958 || 09123435487 || 9848848488 </span>
+        </div>
+        <div className="flex gap-2 text-sm text-text-disabled">
+          <Mail />
+          <span> emailaddress@gmail.com </span>
+        </div>
+        </div>
+      </div>
+      <div className="socials flex gap-2 mt-4">
+        <div className="circle rounded-full p-2 border border-black dark:border-white w-fit">
+          <FbIcon />
+        </div>
+        <div className="circle rounded-full p-2 border border-black dark:border-white   w-fit">
+          <TwitterIcon />
+        </div>
+        <div className="circle rounded-full p-2 border border-black dark:border-white w-fit">
+          <InstagramIcon />
+        </div>
+        <div className="circle rounded-full p-2 border border-black dark:border-white w-fit">
+          <SocialWebIcon />
+        </div>
+      </div>
+
+      {/* Joined */}
+      <div className="flex flex-col gap-2 mt-4">
+      <div className="flex flex-col gap-2 mt-4">
+        {companyStats.map((stat, index) => (
+          <div key={index} className="flex items-center justify-between gap-2">
+            <p className="text-sm text-text-label">{stat.label}</p>
+            <p className="text-sm text-text-primary">{stat.value}</p>
+          </div>
+        ))}
+      </div>
       </div>
     </div>
   )
