e7110431526089c4793eda45a63e97d2bca489e9
Onos <eonosereme@gmail.com>
2024-11-09 19:42:22 +0100

    a

--- app/(nav)/reports/tracking/[userId]/export/page.tsx
+++ app/(nav)/reports/tracking/[userId]/export/page.tsx
@@ -5,9 +5,22 @@ import ExportPageFooter from "@/components/reports/export-page-footer";
 import CustomTable from "@/components/Table/table";
 import ExportPageHeader from "@/components/reports/export-page-header";
 import { empty } from "@/app/config";
-import { staffActivitiesTableFields, activitiesTableData } from "../../data";
+import { trackingTableFields } from "../../data";
 
-const StaffActivitiesExportPage = () => {
+const UserActivitiesExportPage = () => {
+  const generateTableData = (numItems: number) => {
+    return Array.from({ length: numItems }, (_, index) => ({
+      id: index + 1,
+      username: `User ${index + 1}`,
+      page_visited: `Landlord Page ${index + 1}`,
+      action_taken: `Login successful ${index + 1}`,
+      ip_address: `IP ${index + 1}`,
+      location: `Location ${index + 1}`,
+      date: "12/12/12",
+      time: "3:20pm",
+    }));
+  };
+  const tableData = generateTableData(10);
   return (
     <div className="space-y-9 pb-[100px]">
       <BackButton as="p">Barrister Ademola Adedeji</BackButton>
@@ -21,14 +34,11 @@ const StaffActivitiesExportPage = () => {
       <h1 className="text-center text-black dark:text-white text-lg md:text-xl lg:text-2xl font-medium">
         Summary
       </h1>
-      <CustomTable
-        fields={staffActivitiesTableFields}
-        data={activitiesTableData}
-      />
+      <CustomTable fields={trackingTableFields} data={tableData} />
       <Signature />
       <ExportPageFooter />
     </div>
   );
 };
 
-export default StaffActivitiesExportPage;
+export default UserActivitiesExportPage;
