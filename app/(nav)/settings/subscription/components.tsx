64bec1ee4fd2879b38b769a0dc5f52d660568dfb
mubarakbdlrf <mubarakbdlrf@gmail.com>
2024-11-12 08:20:58 +0000

    modals

--- app/(nav)/settings/subscription/components.tsx
+++ app/(nav)/settings/subscription/components.tsx
@@ -1,19 +1,51 @@
-import Image from 'next/image'
+'use client'
+import Button from '@/components/Form/Button/button'
+import ModalPreset from '@/components/Modal/modal-preset'
+import useSubscriptionStore from '@/store/subscriptionStore'
 
 
-export const ConfirmModal = ()=> {
+export const ConfirmModal = () => {
+    const { openSuccessModal, openSuccess, closeWarning, closeSuccess } = useSubscriptionStore();
+    const handleSuccess = ()=> {
+        closeWarning()
+        openSuccess()
+    }
+
     return (
-        <div className="w-[326px] bg-white items-center justify-center h-[422px] rounded-lg">
-            <div className="wrap bg-red-500 rounded-full">
-            <div className="text-center">
-                <Image
-                    src='/icons/cancel-icon.svg'
-                    width={35}
-                    height={35}
-                    alt='confirm'
-                />
+        <ModalPreset type='warning'>
+            <p className="text-text-disabled text-sm font-normal">Are you certain you want to proceed with deleting your domain integration? </p>
+            <div className="flex flex-col items-center gap-4">
+                <Button type="button" size="base_medium" className="py-2 px-8" onClick={handleSuccess}>
+                    proceed
+                </Button>
+                <button
+                    onClick={closeSuccess}
+                    className="text-brand-primary text-sm font-medium"
+                >
+                    Back
+                </button>
             </div>
+        </ModalPreset>
+    )
+}
+
+export const SuccessModal = () => {
+    const { closeSuccess } = useSubscriptionStore();
+    return (
+        <ModalPreset type='success'>
+            <p className="text-text-disabled text-sm font-normal">Domain integrations has been successfully removed and updated.</p>
+            <div className="flex flex-col items-center gap-4">
+                <Button type="button" size="base_medium" className="py-2 px-8" onClick={closeSuccess}>
+                    ok
+                </Button>
             </div>
-        </div>
+        </ModalPreset>
+    )
+}
+
+
+export const EditModal = ()=> {
+    return (
+        <div>Edit modal</div>
     )
 }
