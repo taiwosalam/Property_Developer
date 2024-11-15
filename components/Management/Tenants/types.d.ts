88372c0c4c5405e6cd8d98427f21b9c48329f81a
Onos <eonosereme@gmail.com>
2024-11-15 19:16:09 +0100

    a

--- components/Management/Tenants/types.d.ts
+++ components/Management/Tenants/types.d.ts
@@ -5,7 +5,6 @@ export type AddTenantModalOptions =
   | "options"
   | "add-tenant"
   | "add-multiple-users"
-  | "choose-avatar"
   | "invite-single-user"
   | "invite-multiple-users"
   | "add-user-with-id";
