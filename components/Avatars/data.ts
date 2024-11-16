df3d4cc4e9446684c21feae00d96b023dd256e9e
Onos <eonosereme@gmail.com>
2024-11-16 09:09:21 +0100

    a

--- components/Avatars/data.ts
+++ components/Avatars/data.ts
@@ -29,22 +29,19 @@
 //   }
 // };
 
-
-
 const avatarLinks = [
-  'https://pubassets.ourproperty.ng/uploads/gBTaZYUXOch2qrKq5k5F2EdShRihQjYGuxDwOuu6.png',
-  'https://pubassets.ourproperty.ng/uploads/7M10IKK6OGULqivpfmJ7AMYWNb1BAzpboSLtHffM.png',
-  'https://pubassets.ourproperty.ng/uploads/es2Oy2BoX9CmUjhJMfXS2ILNRkrelzY8aGIKddz1.png',
-  'https://pubassets.ourproperty.ng/uploads/oZCKiu5n4yzoiSDoDkRDBQQxOZi57snQXvd8rJHC.png',
-  'https://pubassets.ourproperty.ng/uploads/yZlXfgqcoIV4SSDpvTrbV2M5udRiusWXunoiC9hz.png',
-  'https://pubassets.ourproperty.ng/uploads/YhSZNRte5NqAu8uNOmNwOPyQIZvKxSGRy4JKF5DO.png',
-  'https://pubassets.ourproperty.ng/uploads/VDtHgwc2C6MwrdjFfjf6X0StiabEj2fRllraBsOT.png',
-  'https://pubassets.ourproperty.ng/uploads/l8GSUMmMDVhK8kQxdJH1DEMQcdxYSpeOGEa9JHSV.png',
-  'https://pubassets.ourproperty.ng/uploads/BTNKJxqAzAHjP1vDKUeJKQNkiI8CsLJNbqOJfKzk.png',
-  'https://pubassets.ourproperty.ng/uploads/UIczCGlVxjMMmJZtwkY3BgxoUSNTqbY9stCcX6Wi.png',
-  'https://pubassets.ourproperty.ng/uploads/Bedc5t1a83qOb1Au3dl3N8RKkn7WNrgZpmg8yCER.png',
-  'https://pubassets.ourproperty.ng/uploads/YhSZNRte5NqAu8uNOmNwOPyQIZvKxSGRy4JKF5DO.png',
-  
+  "https://pubassets.ourproperty.ng/uploads/gBTaZYUXOch2qrKq5k5F2EdShRihQjYGuxDwOuu6.png",
+  "https://pubassets.ourproperty.ng/uploads/7M10IKK6OGULqivpfmJ7AMYWNb1BAzpboSLtHffM.png",
+  "https://pubassets.ourproperty.ng/uploads/es2Oy2BoX9CmUjhJMfXS2ILNRkrelzY8aGIKddz1.png",
+  "https://pubassets.ourproperty.ng/uploads/oZCKiu5n4yzoiSDoDkRDBQQxOZi57snQXvd8rJHC.png",
+  "https://pubassets.ourproperty.ng/uploads/yZlXfgqcoIV4SSDpvTrbV2M5udRiusWXunoiC9hz.png",
+  "https://pubassets.ourproperty.ng/uploads/YhSZNRte5NqAu8uNOmNwOPyQIZvKxSGRy4JKF5DO.png",
+  "https://pubassets.ourproperty.ng/uploads/VDtHgwc2C6MwrdjFfjf6X0StiabEj2fRllraBsOT.png",
+  "https://pubassets.ourproperty.ng/uploads/l8GSUMmMDVhK8kQxdJH1DEMQcdxYSpeOGEa9JHSV.png",
+  "https://pubassets.ourproperty.ng/uploads/BTNKJxqAzAHjP1vDKUeJKQNkiI8CsLJNbqOJfKzk.png",
+  "https://pubassets.ourproperty.ng/uploads/UIczCGlVxjMMmJZtwkY3BgxoUSNTqbY9stCcX6Wi.png",
+  "https://pubassets.ourproperty.ng/uploads/Bedc5t1a83qOb1Au3dl3N8RKkn7WNrgZpmg8yCER.png",
+  "https://pubassets.ourproperty.ng/uploads/YhSZNRte5NqAu8uNOmNwOPyQIZvKxSGRy4JKF5DO.png",
 ];
 
 let cachedAvatars: string[] = [];
