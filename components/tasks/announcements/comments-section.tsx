d1db1a453b3aaeef13b5f7c1dd7eb82704fbb05c
mubarak <rafiumubarak2019@gmail.com>
2024-11-21 19:03:29 +0100

    comments

--- components/tasks/announcements/comments-section.tsx
+++ components/tasks/announcements/comments-section.tsx
@@ -1,7 +1,11 @@
-import Comment from "./comment";
+import Comment, { CommentData } from "./comment";
 import { SectionSeparator } from "@/components/Section/section-components";
-import { comments } from "./data";
-const CommentsSection = () => {
+
+interface CommentsSectionProps {
+  comments: CommentData[];
+}
+
+const CommentsSection = ({ comments }: CommentsSectionProps) => {
   return (
     <div>
       <h3 className="text-black dark:text-white text-base font-medium mb-4">
@@ -9,7 +13,11 @@ const CommentsSection = () => {
       </h3>
       {comments.map((comment, index) => (
         <div key={comment.id}>
-          <Comment {...comment} handleLike={() => {}} handleDislike={() => {}} />
+          <Comment 
+            {...comment} handleLike={() => {}} 
+            handleDislike={() => {}} 
+            handleSubmit={async (e) => {}}  
+          />
           {index < comments.length - 1 && <SectionSeparator className="my-4" />}
         </div>
       ))}
