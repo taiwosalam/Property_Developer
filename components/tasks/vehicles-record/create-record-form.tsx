cee290a96ef92a775ad4caf477e796841f9c3426
mubarak <rafiumubarak2019@gmail.com>
2024-12-19 18:38:41 +0100

    thursday

--- components/tasks/vehicles-record/create-record-form.tsx
+++ components/tasks/vehicles-record/create-record-form.tsx
@@ -11,6 +11,7 @@ import useVehicleRecordStore from "@/store/vehicle-record";
 import { createVehicleRecord } from "./data";
 import { toast } from "sonner";
 import { useEffect, useState } from "react";
+import Select from "@/components/Form/Select/select";
 
 
 
@@ -70,12 +71,20 @@ const CreateRecordForm = () => {
             loading={loading}
           />
         ) : (
-          <Input
-            required
-            label="Input Guest/Visitor ID"
+          <div className="grid gap-4 md:gap-5 md:grid-cols-2 lg:grid-cols-3">
+            <div className="flex flex-col gap-2">
+              <label> Input Guest/Visitor ID </label>
+              <div className="flex items-center pr-2 h-12 text-xs md:text-sm font-normal rounded-[4px] w-full custom-primary-outline border border-solid border-[#C1C2C366] bg-neutral-2 dark:bg-darkText-primary hover:border-[#00000099] dark:hover:border-darkText-2">
+              <input type="text" className="w-full h-full rounded-[4px] outline-none px-2" />
+              <button className='bg-brand-9 text-xs rounded-md px-2 text-white h-3/4'> Submit </button>
+              </div>
+            </div>
+          <Select
+            label="Select From Record"
             id="guest_id"
-            inputClassName="rounded-lg"
+            options={['Tenant 1', 'Tenant 2', 'Tenant 3']}
           />
+          </div>
         )}
       </div>
       <div className="space-y-4">
