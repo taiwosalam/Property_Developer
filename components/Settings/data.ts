5c05bdc0da3e2d49590069e6f3d3b65760e02da8
Ola-Oluwajuwon <okayode986@stu.ui.edu.ng>
2025-01-17 07:46:51 -0800

    settings

--- components/Settings/data.ts
+++ components/Settings/data.ts
@@ -5,7 +5,11 @@ import type { SettingsServiceOwners, TenantOccupantTier } from "./types";
 export const settings_link_tabs = [
   "profile",
   "management",
+  "subscription",
+  "services",
   "security",
+  "enrollment",
+  "appearance",
   "others",
 ] as const;
 
