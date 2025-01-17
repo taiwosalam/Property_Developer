9e5a0a3797127eb2711d20390b74eb9f7a46fc1c
Ola-Oluwajuwon <okayode986@stu.ui.edu.ng>
2025-01-17 07:31:13 -0800

    applications page

---
+++ app/(nav)/manager/applications/page.tsx
@@ -0,0 +1,60 @@
+"use client";
+import React from "react";
+
+// Imports
+import ApplicationCard from "@/components/Applications/application-card";
+import AutoResizingGrid from "@/components/AutoResizingGrid/AutoResizingGrid";
+import ManagementStatistcsCard from "@/components/Management/ManagementStatistcsCard";
+import FilterBar from "@/components/FIlterBar/FilterBar";
+import { DocumentssFilterOptionsWithDropdown } from "../documents/data";
+
+const Applications = () => {
+  return (
+    <div className="custom-flex-col gap-8">
+      <div className="hidden md:flex gap-5 flex-wrap">
+        <ManagementStatistcsCard
+          title="Total Application"
+          newData={34}
+          total={657}
+          colorScheme={1}
+        />
+        <ManagementStatistcsCard
+          title="Mobile Application"
+          newData={34}
+          total={657}
+          colorScheme={2}
+        />
+        <ManagementStatistcsCard
+          title="Web Application"
+          newData={34}
+          total={657}
+          colorScheme={3}
+        />
+      </div>
+      <div className="custom-flex-col gap-5">
+        <FilterBar
+          azFilter
+          isDateTrue
+          hasGridListToggle={false}
+          pageTitle="Applications"
+          aboutPageModalData={{
+            title: "Applications",
+            description:
+              "This page contains a list of Applications on the platform.",
+            video: "",
+          }}
+          searchInputPlaceholder="Search application"
+          handleFilterApply={() => {}}
+          filterOptionsMenu={DocumentssFilterOptionsWithDropdown}
+        />
+        <AutoResizingGrid minWidth={300} gap={32}>
+          <ApplicationCard status="flagged" type="staff" />
+          <ApplicationCard status="unflagged" type="guest" />
+          <ApplicationCard status="unflagged" type="staff" />
+        </AutoResizingGrid>
+      </div>
+    </div>
+  );
+};
+
+export default Applications;
