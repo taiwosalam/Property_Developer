ed904b005f46d0dad93de6fee5562ff4900bfe1b
Bolaji06 <joshspencer0603@gmail.com>
2025-06-25 10:54:33 +0100

    examine report

--- app/(nav)/tasks/examine/[examineId]/report/page.tsx
+++ app/(nav)/tasks/examine/[examineId]/report/page.tsx
@@ -7,139 +7,173 @@ import Button from "@/components/Form/Button/button";
 import ExportPageHeader from "@/components/reports/export-page-header";
 import { SectionContainer } from "@/components/Section/section-components";
 import { LandlordTenantInfoBox } from "@/components/Management/landlord-tenant-info-components";
+import { examineService, inspectionCheckList } from "../manage/data";
+import useFetch from "@/hooks/useFetch";
+import { useParams } from "next/navigation";
+import ServerError from "@/components/Error/ServerError";
+import NetworkError from "@/components/Error/NetworkError";
+import PageCircleLoader from "@/components/Loader/PageCircleLoader";
+import { ExamineReportApiResponse } from "./type";
+import { IExamineReportPageData, transformExamineReportPageData } from "./data";
+import { useEffect, useRef, useState } from "react";
+import ExportPageFooter from "@/components/reports/export-page-footer";
 
 const ExamineReportpage = () => {
+  const params = useParams();
+  const paramId = params?.examineId as string;
+
+  const exportRef = useRef<HTMLDivElement | null>(null);
+
+  const [pageData, setPageData] = useState<IExamineReportPageData | null>(null);
+
   const commonBoxStyle: React.CSSProperties = {
     boxShadow:
       "0px 1px 2px 0px rgba(21, 30, 43, 0.08), 0px 2px 4px 0px rgba(13, 23, 33, 0.08)",
   };
   const commonBoxClassName = "py-6 px-4 rounded-lg space-y-2";
+
+  const {
+    data: apiData,
+    loading,
+    silentLoading,
+    error,
+    isNetworkError,
+  } = useFetch<ExamineReportApiResponse>(`/examine/${paramId}`);
+
+  useEffect(() => {
+    if (apiData) {
+      const transformData = transformExamineReportPageData(apiData);
+      setPageData(transformData);
+    }
+  }, [apiData]);
+
+  const summaryLookup = (pageData?.inspection_summary || []).reduce(
+    (acc, obj) => {
+      if (obj && typeof obj === "object") {
+        const [key, value] = Object.entries(obj)[0];
+        acc[key] = value;
+      }
+      return acc;
+    },
+    {} as Record<string, string>
+  );
+  const checkListLookup = (pageData?.inspection_checklist || []).reduce(
+    (acc, obj) => {
+      if (obj && typeof obj === "object") {
+        const [key, value] = Object.entries(obj)[0];
+        acc[key] = value;
+      }
+      return acc;
+    },
+    {} as Record<string, string>
+  );
+
+  if (loading) <PageCircleLoader />;
+  if (error) <ServerError error={error} />;
+  if (isNetworkError) <NetworkError />;
+
   return (
     <div className="space-y-8 pb-[100px]">
       <BackButton as="p">Back</BackButton>
-      <ExportPageHeader />
-      <h1 className="text-black dark:text-white text-lg md:text-xl lg:text-2xl font-medium text-center">
-        Property Inspection Report
-      </h1>
-      <div className="grid md:grid-cols-2 gap-8">
-        <LandlordTenantInfoBox
-          className={`${commonBoxClassName}`}
-          style={commonBoxStyle}
-        >
-          <div className="flex items-center justify-between gap-2">
-            <p className="text-text-tertiary dark:text-darkText-1 text-[16px] font-medium">
-              Inspected Date:
+      <div className="space-y-8 pb-[100px]" ref={exportRef}>
+        <ExportPageHeader />
+        <h1 className="text-black dark:text-white text-lg md:text-xl lg:text-2xl font-medium text-center">
+          Property Inspection Report
+        </h1>
+        <div className="grid md:grid-cols-2 gap-8">
+          <LandlordTenantInfoBox
+            className={`${commonBoxClassName}`}
+            style={commonBoxStyle}
+          >
+            <div className="flex items-center justify-between gap-2">
+              <p className="text-text-tertiary dark:text-darkText-1 text-[16px] font-medium">
+                Inspected Date:
+              </p>
+              <p className="text-sm font-medium text-text-secondary dark:text-darkText-2 text-right">
+                {pageData?.inspection_date}
+              </p>
+            </div>
+            <div className="flex items-start justify-between gap-2">
+              <p className="text-text-tertiary dark:text-darkText-1 text-[16px] font-medium">
+                Added Guest:
+              </p>
+              <p className="text-sm font-medium text-text-secondary dark:text-darkText-2 text-right">
+                {pageData?.guest}
+              </p>
+            </div>
+          </LandlordTenantInfoBox>
+          <LandlordTenantInfoBox
+            className={`${commonBoxClassName}`}
+            style={commonBoxStyle}
+          >
+            <p className="text-base font-medium text-text-tertiary dark:text-darkText-1">
+              Description
             </p>
-            <p className="text-sm font-medium text-text-secondary dark:text-darkText-2 text-right">
-              8 -11th January 2024
-            </p>
-          </div>
-          <div className="flex items-start justify-between gap-2">
-            <p className="text-text-tertiary dark:text-darkText-1 text-[16px] font-medium">
-              Added Guest:
-            </p>
-            <p className="text-sm font-medium text-text-secondary dark:text-darkText-2 text-right">
-              Landlord
-              <br />
-              Mr Ajadi David
-            </p>
-          </div>
-        </LandlordTenantInfoBox>
-        <LandlordTenantInfoBox
-          className={`${commonBoxClassName}`}
-          style={commonBoxStyle}
-        >
-          <p className="text-base font-medium text-text-tertiary dark:text-darkText-1">
-            Description
-          </p>
-          <p className="text-sm font-medium text-text-secondary dark:text-darkText-2">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-            eget dictum sem, ut molestie eros. Morbi in dolor augue. Sed aliquet
-            ipsum fringilla sapien facilisis consectetur.
-          </p>
-        </LandlordTenantInfoBox>
-      </div>
-      <SectionContainer heading="Service connected to property">
-        <AutoResizingGrid minWidth={340}>
-          {["Electricity", "Sewer", "Gas", "Drainage", "Water"].map(
-            (service, index) => (
-              <span key={index}>{service}</span>
-            )
-          )}
-        </AutoResizingGrid>
-      </SectionContainer>
-      <SectionContainer heading="Site Summary">
-        <AutoResizingGrid minWidth={340}>
-          {[
-            "Age of Building",
-            "Construction Type",
-            "Roof",
-            "Condition",
-            "Extension/ Renovation",
-            "Out Buildings",
-            "Sub Floor",
-            "Site",
-            "Compare to others",
-          ].map((item, index) => (
-            <ExamineKeyValue key={item + index} itemKey={item} value="N/A" />
-          ))}
-        </AutoResizingGrid>
-      </SectionContainer>
-      <SectionContainer heading="Inspection Checklist">
-        <AutoResizingGrid minWidth={340}>
-          {[
-            "Outdoor Steps and Sidewalk",
-            "Dining Rooms",
-            "Outdoor Paint",
-            "Den(s)",
-            "Driveway",
-            "Study Room(s)",
-            "Storage Room(s)",
-            "Outdoor Plantation",
-            "Outdoor Entry Way",
-            "Game Room(s)",
-            "Doors",
-            "Music Room(s)",
-            "Door Fixtures",
-            "Fireplaces",
-            "Flooring",
-            "Carpentry",
-            "Bathroom Tiles",
-            "Bathroom Faucets",
-            "Window",
-            "Water Pressure",
-            "Window Screen",
-            "Kitchen",
-            "Window Fixtures",
-            "Laundry",
-            "Window Furnishing",
-            "Lighting",
-            "Ceilings",
-            "Disposal",
-            "Light Fixtures",
-            "Shelving",
-            "Staircases",
-            "Bedrooms",
-            "Indoor Paint",
-            "Wardrobes and Closets",
-            "Electrical Outlets and Fixtures",
-            "Living Room",
-          ].map((item, index) => (
-            <ExamineKeyValue key={item + index} itemKey={item} value="N/A" />
-          ))}
-        </AutoResizingGrid>
-      </SectionContainer>
-      <SectionContainer heading="Inspection Summary Note">
-        <p className="text-text-secondary dark:text-darkText-2 text-[16px] font-normal">
-          House is in good condition
-        </p>
-      </SectionContainer>
+            {pageData?.description && (
+              <div
+                className="text-sm font-medium text-text-secondary dark:text-darkText-2"
+                dangerouslySetInnerHTML={{ __html: pageData.description }}
+              />
+            )}
+          </LandlordTenantInfoBox>
+        </div>
 
-      <FixedFooter className="flex justify-end">
-        <Button size="base_medium" variant="sky_blue" className="py-2 px-6">
-          Print
-        </Button>
-      </FixedFooter>
+        <SectionContainer heading="Service connected to property">
+          {pageData && pageData?.services?.length > 0 ? (
+            <AutoResizingGrid minWidth={220}>
+              {pageData?.services?.map((service, index) => (
+                <span key={index}>{service}</span>
+              ))}
+            </AutoResizingGrid>
+          ) : (
+            "--- ---"
+          )}
+        </SectionContainer>
+
+        <SectionContainer heading="Site Summary">
+          <AutoResizingGrid minWidth={340}>
+            {[
+              "Age of Building",
+              "Construction Type",
+              "Roof",
+              "Condition",
+              "Extension/ Renovation",
+              "Out Buildings",
+              "Sub Floor",
+              "Site",
+              "Compare to others",
+            ].map((item, index) => (
+              <ExamineKeyValue
+                key={item + index}
+                itemKey={item}
+                value={summaryLookup[item] || "N/A"}
+              />
+            ))}
+          </AutoResizingGrid>
+        </SectionContainer>
+        <SectionContainer heading="Inspection Checklist">
+          <AutoResizingGrid minWidth={340}>
+            {inspectionCheckList.map((item, index) => (
+              <ExamineKeyValue
+                key={item + index}
+                itemKey={item}
+                value={checkListLookup[item] || "N/A"}
+              />
+            ))}
+          </AutoResizingGrid>
+        </SectionContainer>
+        <SectionContainer heading="Inspection Summary Note">
+          {pageData && (
+            <div
+              className="text-text-secondary dark:text-darkText-2 text-[16px] font-normal"
+              dangerouslySetInnerHTML={{
+                __html: pageData?.summary_note ?? "--- ---",
+              }}
+            />
+          )}
+        </SectionContainer>
+      </div>
+      <ExportPageFooter printRef={exportRef} noBack={false}/>
     </div>
   );
 };
