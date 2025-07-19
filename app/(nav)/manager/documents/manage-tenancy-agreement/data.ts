f1c70e6d91684d7b3825e9535febcda1480b5fb8
BinaryTechoo1 <mubarakbdlrf@gmail.com>
2025-07-19 16:56:50 +0100

    fixes - document

---
+++ app/(nav)/manager/documents/manage-tenancy-agreement/data.ts
@@ -0,0 +1,77 @@
+import api, { handleAxiosError } from "@/services/api";
+import { toast } from "sonner";
+
+export interface DocumentArticle {
+  title: string;
+  content: string;
+  section: string;
+  summary: string;
+}
+
+export interface LawyerDetails {
+  type: string;
+  id: number;
+  category: string;
+  property_type: string;
+  property_sub_type: string;
+  lawyer_fullname: string;
+  lawyer_office_address: string;
+  lawyer_firm_name: string;
+  lawyer_email: string;
+  lawyer_phone_number: string;
+  lawyer_legal_seal: string;
+  lawyer_signature: string;
+}
+
+export interface PropertyDocument {
+  document_id: number;
+  property_name: string;
+  property_description: string;
+  property_address: string;
+  landlord_name: string;
+  landlord_id: number | string;
+  landlord_type: string;
+  document_type: string;
+  property_id: number;
+  total_unit: number;
+  created_date: string;
+  articles: DocumentArticle[];
+  document: LawyerDetails;
+}
+
+export interface ManageDocumentsAPIResponse {
+  status: string;
+  document: PropertyDocument;
+}
+
+export interface CheckboxOption {
+  title: string;
+  value: string;
+  description: string;
+  section: string;
+  summary: string;
+}
+
+export const transformDocumentArticleResponse = (
+  response: ManageDocumentsAPIResponse
+): CheckboxOption[] => {
+  return response.document.articles.map((article) => ({
+    title: article.title,
+    value: article.title.toLowerCase().replace(/\s+/g, "_"),
+    description: article.content,
+    section: article.section,
+    summary: article.summary,
+  }));
+};
+
+export const deleteDocument = async (documentId: number) => {
+  try {
+    const res = await api.delete(`/property-document/${documentId}`);
+    if (res.status === 200) {
+      return true;
+    }
+  } catch (error) {
+    handleAxiosError(error)
+    return false;
+  }
+};
