14668f8c0145550bd0204c38d43e6cbde005e99e
unknown <ejeanobionyeka@gmail.com>
2025-08-23 12:00:14 +0100

    maded changes to the layout because of the marquee

--- app/(nav)/layout.tsx
+++ app/(nav)/layout.tsx
@@ -228,9 +228,9 @@ const NavLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
           {/* <div className="pt-2 bg-white dark:bg-[#020617]">
           <SmartMarquee />
           </div> */}
-          <div className="pt-2 bg-white dark:bg-[#020617]">
-            <SmartMarquee />
-          </div>
+          <>
+            <SmartMarquee className="pt-2 bg-white dark:bg-[#020617]"/>
+          </>
           <div
             className={`h-[50px] px-3 flex items-center ${navbar !== "row" ? "justify-between" : "justify-end"
               } gap-2 bg-white dark:bg-[#020617] max-w-full`}
@@ -285,7 +285,7 @@ const NavLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
           }}
           className={clsx(
             // "px-2 sm:px-3 md:p-6 bg-neutral-2 dark:bg-[#000000] relative z-[1] duration-300 mt-[48px] min-h-[calc(100vh-152px)]",
-            "px-2 sm:px-3 md:p-6 bg-neutral-2 dark:bg-[#000000] relative z-[1] duration-300 min-h-[calc(100vh-152px)]",
+            "px-2 sm:px-3 md:p-6 bg-neutral-2 pt-16 md:mt-20 dark:bg-[#000000] relative z-[1] duration-300 min-h-[calc(100vh-152px)]",
             {
               "w-full md:ml-0 lg:ml-0": navbar === "row",
               "md:ml-[110px] lg:ml-[110px]": !isSideNavOpen && navbar !== "row",
@@ -296,7 +296,7 @@ const NavLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
               "md:opacity-100 md:pointer-events-auto": navbar !== "row",
             },
             // Conditional margin-top based on user role
-            role === "account" || role === "user" ? "mt-14" : "mt-[58px]"
+            // role === "Branch Manager" || role === "Account Manager" || role === "Staff" ? "pt-24 md:mt-14" : "mt-[58px]"
           )}
         >
           {children}
